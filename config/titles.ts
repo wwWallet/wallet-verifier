914c296a419b19c1a7f60b6e41b1f6b59698cd20
Vasileios Markopoulos <vmarkop@gunet.gr>
2026-06-10 17:20:14 +0300

    Move page titles to locale

--- config/titles.ts
+++ config/titles.ts
@@ -1,23 +1,12 @@
 // titles.ts
 import { config } from ".";
+import locale from "./locale";
 
 export const siteTitle = config?.siteConfig?.name || "wwWallet Verifier";
 
-export const titles: Record<string, string> = {
-	"index": siteTitle,
-	"manage-certificates": "Manage Trusted Certificates",
-	"public-definitions": "Credential Verification Requests",
-	"handle-response-code": "Processing Verification Response",
-	"error": "Error",
-	"presentation-success": "Credential Verification Results",
-	"request-credentials": "Select Credential Claims",
-	"request-custom-credential": "Create Custom Credential Request",
-	"presentation-request": "Present Credentials",
-};
-
-export function titleWithSiteName(titleKey: string): string {
-	if (Object.keys(titles).includes(titleKey)) {
-		return `${titles[titleKey]} | ${siteTitle}`;
+export function titleWithSiteName(titleKey: string, currentLocale = "en"): string {
+	if((locale as any)[currentLocale].pageTitles[titleKey]) {
+		return `${(locale as any)[currentLocale].pageTitles[titleKey]} | ${siteTitle}`;
 	}
 	return siteTitle;
 }
